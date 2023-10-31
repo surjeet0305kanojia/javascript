ba7ee4b8c212ac52ef6c2ef0b1be4ede8f59de0f
Surjeet kumar kanojia <surjeet0305kanojia@gmail.com>
2023-10-31 05:49:19 +0000

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict";  //  use to declare that we use latest javascript standard
+// for documentation "mdn" or tc39.es -> official docs 
+
+// int -> number
+// bigint -> big  number
+// string-> "......"
+// boolean-> true or false
+// null -> standalone value;
+// undefined -> not defined;
+// symbol -> unique
+
+// object
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
