969e269af29aba0e73afd0ac798e98b782432138
Surjeet kumar kanojia <surjeet0305kanojia@gmail.com>
2023-10-31 06:57:33 +0000

    conversion

---
+++ basics/03_coversion.js
@@ -0,0 +1,38 @@
+//  conversion to numbers
+
+let score ="33ans"
+
+// console.log(typeof score);
+score1=Number(score)
+// console.log(typeof score1);
+// console.log(score1);   
+// NaN -> when a string is converted to number but actually its not a number then NaN is introduced
+// str="asdfg"-> NaN
+// if value is null -> 0
+// if undefined -> NaN
+// true-> 1; false -> 0
+
+/* conversion to Boolean*/
+let value=" ";
+let boolvalue=Boolean(value)
+// console.log( typeof boolvalue);
+// console.log( boolvalue);
+
+/* if 0-> false else true for any number
+Nan -> false
+null-> false
+"" empty string-> false
+" " space -> true
+"asdf" -> true
+*/ 
+
+
+/* conversion to string */
+let str=
+string=String(str);
+console.log( typeof string);
+console.log(  string);
+
+
+
+
