e46878d9d0f2357b067c47ac6d182a65a63bc80b
Surjeet kumar kanojia <surjeet0305kanojia@gmail.com>
2023-11-05 06:26:21 +0000

    string

---
+++ basics/04_strings.js
@@ -0,0 +1,18 @@
+let name ="surjeet"
+let age ='21'
+
+console.log("my name is "+name+". I am "+age+" years old.")
+// modern way to console using backtics.....String impotalation
+console.log(`my name is ${name} and I'm ${age} years old`);
+
+// another method to declare string
+let string = new String('Surjeet');
+console.log(string.toLocaleUpperCase());
+console.log(string.indexOf('j'));
+console.log(string.substring(0,5));
+console.log(string.slice(4,7));
+
+let s1='    suru   '
+console.log(s1);
+console.log(s1.trim());
+console.log(s1.replace("u",'x'));
