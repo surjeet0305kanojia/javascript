8b25c45149bda993434f6caad2d3a0e635439260
Surjeet kumar kanojia <surjeet0305kanojia@gmail.com>
2023-11-18 19:40:45 +0000

    math

---
+++ basics/05_math.js
@@ -0,0 +1,21 @@
+const score =400
+const val= new Number(500);
+// console.log(score);
+// console.log(val);
+// console.log(val.toString().length);
+// console.log(val.toFixed(2));
+
+value =1000000000
+// console.log(value.toLocaleString("en-IN"));
+
+
+// ---------------------------------------Maths---------------------------------------
+console.log(Math.random());
+console.log(Math.random()*10+1);
+console.log(Math.floor(Math.random()*10+1));
+
+// to get random value from a range 
+min=10
+max=30
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
